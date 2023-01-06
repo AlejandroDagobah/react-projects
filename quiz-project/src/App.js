01dad1a4788f03a695cf6d08fefa4e42e94a604e
dagobah <alejandrodagobah@gmail.com>
2023-01-05 23:17:09 -0500

    Quizzical App finalized

--- quiz-project/src/App.js
+++ quiz-project/src/App.js
@@ -1,27 +1,118 @@
-import React from "react";
-import IntroView from "./components/IntroView";
-import QuizView from "./components/QuizView" 
+import React from "react"
+import IntroView from "./components/IntroView"
+import Question from "./components/Question"
+
+import { nanoid } from "nanoid"
 
 export default function App() {
   const [mainView, setMainView] = React.useState(true)
   const [questions, setQuestions] = React.useState([])
   const [quizState, setQuizState] = React.useState(0)
-
+  const [answers, setAnswers] = React.useState([])
+  const [score, setScore] = React.useState(0)
+  const [finalized, setFinalization] = React.useState(false)
+    
   React.useEffect(function(){
 
+    function decodeText(text) {
+      const parser = new DOMParser;
+      const dom = parser.parseFromString(
+          '<!doctype html><body>' + text,
+          'text/html');
+      return dom.body.textContent;
+    }
       fetch('https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple')
       .then(response => response.json())
       .then((data) => {      
-        setQuestions(data.results)
+        setQuestions(()=>{
+          const simplified = data.results.map(question =>{
+
+            let answers = [...question.incorrect_answers, question.correct_answer]
+
+            answers = answers.map(item => decodeText(item))
+            
+
+            answers.sort(() =>{
+              return Math.random() - 0.5
+            })
+       
+
+            return {question: decodeText(question.question), answers: answers, correct_answer: decodeText(question.correct_answer), question_id: nanoid()}
+          })
+
+          return simplified
+
+        })
       })
   
     }, [quizState])
 
+
+  function addAnswer(questionId, answeredQ) {
+    setAnswers(prevAnswers =>{
+      if(prevAnswers.some(answer=>answer.id === questionId)){
+        return prevAnswers.map(answer=>{
+          return answer.id === questionId ? {...answer, value:answeredQ} : answer 
+        })
+      }else{
+        return [...prevAnswers, {id:questionId, value:answeredQ}]
+      }
+    })
+
+  }
+
+  function compareAnswers(e) {
+    e.preventDefault()
+
+    for (let i = 0; i < answers.length; i++) {
+        const answer = answers[i];
+        const question = questions[i];
+
+
+        if(answer.id === question.question_id)
+        {
+
+          console.log(question)
+          if(answer.value === question.correct_answer){
+            console.log("respuesta Correcta:", answer.value)
+            setScore(prevScore=>prevScore += 1)
+  
+          }else{
+            console.log("respuesta Incorrecta:", question.correct_answer)
+  
+          }
+        }
+    }
+
+    setFinalization(true)
+
+  }
+
+  console.log("YOUR FINAL SCORE IS:", score)
+
+  let questionsElements = questions.map(question =>{       
+  
+    return <Question key={question.question_id} question={question.question} answers={question.answers} addAnswer={addAnswer} id={question.question_id} correct_answer={question.correct_answer} isFinalized={finalized}/>
+
+  })
+  
   return (
     
     <div className="App">
       <div className="container">
-        {mainView === true ? <IntroView onClickTrigger={() => setMainView(false)}/> : <QuizView questions={questions}/>}
+        {mainView === true ? 
+          <IntroView onClickTrigger={() => setMainView(false)}/> : 
+
+          <div>
+            <form>
+                {questionsElements}
+
+                {finalized && <b className="score">You scored {score}/{questions.length} correct answers</b>}
+                <button className="submit-btn" onClick={finalized ? () => setQuizState(0) : (e) => compareAnswers(e)}>{finalized ? 'Play Again' : 'Check answers'}</button>
+            </form>
+          </div>
+        }
+        
       </div>
     </div>
   );
--- quiz-project/src/components/Question.js
+++ quiz-project/src/components/Question.js
@@ -3,16 +3,7 @@ import React from "react";
 
 export default function Question(props) {
 
-
-    const [answerButtons, setAnswerButtons] = React.useState([
-        {value: props.answers[0], isCorrect:"", isClicked: false},
-        {value: props.answers[1], isCorrect:"", isClicked: false},
-        {value: props.answers[2], isCorrect:"", isClicked: false},
-        {value: props.answers[3], isCorrect:"", isClicked: false}
-    ])
-
-
- 
+    const [answerButtons, setAnswerButtons] = React.useState(props.answers.map(answer=>({answer:answer})))
 
     function clickAnswer(e) {
         e.preventDefault()
@@ -20,44 +11,31 @@ export default function Question(props) {
         const button = e.target
 
         setAnswerButtons(prevAnswer => {
+            
+            return prevAnswer.map(answer=>{
 
-            let newAnswers = []
-               
-            for (let i = 0; i < prevAnswer.length; i++) {
-                const answer = prevAnswer[i];
+                return answer.answer == button.value ? {answer:button.value, isClicked: true} : {answer:answer.answer, isClicked: false} 
 
-                if(answer.value == button.value){
-                    newAnswers.push({...answer, isClicked: true})
-
-
-                }else{
-                    newAnswers.push({...answer, isClicked: false})
-
-                }
-            }
-           
-            return newAnswers
-
+            })
         })
 
-
-        
-        props.setAnswersSubmitted(prevAnswersSubmited =>{
-
-            const newAnswers = []
-
-
-            return [...prevAnswersSubmited]
-
-        })
-
-
+        props.addAnswer(props.id, button.value)
     }
-    
-    
+
 
     let buttons = answerButtons.map(answer =>{
-        return <button key={nanoid()} style={answer.isClicked ? {backgroundColor: '#D6DBF5'} : {backgroundColor: '#ffffff00'}} value={answer.value} onClick={(e) => clickAnswer(e)}>{answer.value}</button>
+        let styles = {backgroundColor:"#ffffff00"}
+
+        if(props.isFinalized == false){
+            styles.backgroundColor = answer.isClicked && '#D6DBF5'
+            
+        }else if(answer.isClicked && props.isFinalized == true){
+            styles.backgroundColor = props.correct_answer === answer.answer ? '#94D7A2' : "#F8BCBC"
+
+        }else if(props.isFinalized == true){
+            styles.backgroundColor = answer.isClicked == false && props.correct_answer === answer.answer ? '#94D7A2' : "#ffffff00"
+        }
+        return <button className="button-answer" key={nanoid()} style={styles} value={answer.answer} onClick={(e) => clickAnswer(e)}>{answer.answer}</button>
     })
 
 
